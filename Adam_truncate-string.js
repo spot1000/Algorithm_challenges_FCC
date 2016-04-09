b07da942f9e230acf8fb0a5f954ad98b20595dd2
Adam Lichter <adamlichter@gmail.com>
2016-04-09 10:47:27 -0230

    Update and rename Adam_truncate-string to Adam_truncate-string.js

--- Adam_truncate-string.js
+++ Adam_truncate-string.js
@@ -1,13 +1,12 @@
 function truncateString(str, num) {
+  
   if (num <= 3) {
     return str.slice(0, num - str.length) + "...";
   }
-  else {
-    if (num >= str.length) {
+  else if (num >= str.length) {
       return str;
-    }
-    else {
+  }
+  else {
       return str.slice(0, num - str.length - 3) + "...";
-    }
   }
 }
